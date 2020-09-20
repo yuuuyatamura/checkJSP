ad8a9bb36a1ebe0b860d0261421b34b7a476627b
yuuuyatamura <yt0829xwp@gmail.com>
2020-09-20 17:56:45 +0900

    simple Application Start

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.trySpring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //ポイント1:@Controller
 @Controller
@@ -12,5 +15,17 @@ public class HelloController {
 	public String getHello() {
 		//hello.htmlに画面遷移
 		return "hello";
+	}
+	
+	//ポイント1:@PostMapping
+	//ポイント2:@RequestMapping
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1")String str,Model model) {
+		//ポイント3:moldel.addAttribute
+		//画面から受け取った文字列をModelに登録
+		model.addAttribute("sample", str);
+		
+		//helloResponse.htmlに画面遷移
+		return "helloResponse";
 	}
 }
