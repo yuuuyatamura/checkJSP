1b3541d100d4cf8e10b73aa9c2f00c08235b29a9
yuuuyatamura <yt0829xwp@gmail.com>
2020-09-21 21:25:48 +0900

    AOP実装完了

--- src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.example.demo.login.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,14 +12,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 	//AOPの実装
-	@Before("execution(* com.example.demo.login.controller.LoginController.getLogin (..))")
-	public void startLog(JoinPoint jp) {
+	@Around("execution(* *.*.*Controller.* (..))")
+	public Object startLog(ProceedingJoinPoint jp)throws Throwable {
 		System.out.println("メソッド開始: "+jp.getSignature());
-	}		
-	//ポイント2:AOPの実装
-	//execution(<戻り値><パッケージ名>.<クラス名>.<メソッド名>(<引数>))
-	@After("execution(* com.example.demo.login.controller.LoginController.getLogin (..))")
-	public void endLog(JoinPoint jp) {
-		System.out.println("メソッド開始: "+jp.getSignature());
+
+		try {
+			//ポイント2：メソッド実行
+			Object result = jp.proceed();
+			System.out.println("メソッド終了:"+ jp.getSignature());
+			return result;
+		}catch (Exception e) {
+			System.out.println("メソッド異常終了 : "+jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
 	}
 }
